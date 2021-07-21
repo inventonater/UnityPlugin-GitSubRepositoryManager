985829f40353d8f4094e36749c705cd5c0cf2259
Ross Borchers <ross@renderheads.com>
2021-07-21 12:36:41 +0200

    Fixed some bugs with status message parsing. Safer locking

--- Assets/Package/Core/Repository.cs
+++ Assets/Package/Core/Repository.cs
@@ -72,80 +72,17 @@ namespace GitRepositoryManager
 
 		private string _lastStatus = string.Empty;
 		private string _lastPrintableStatus = string.Empty;
-		public string PrintableStatus => _lastPrintableStatus;
-
-		private List<KeyValuePair<string, string>> _statusPrettifyLookup = new List<KeyValuePair<string, string>>()
+		public string PrintableStatus
 		{
-			new KeyValuePair<string, string>("D ", "Deleted"),
-			new KeyValuePair<string, string>("?? ", "Untracked"),
-			new KeyValuePair<string, string>("M ", "Modified")
-			
-		};
-
-		///This is called from multiple threads! May block.
-		public string CreatePrintableStatus(bool collapse)
-		{
-			lock(_lastStatus)
+			get
 			{
-				if (_lastStatus == "Repository not found.")
+				lock (statusLockObject)
 				{
-					return _lastStatus;
+					return _lastPrintableStatus;
 				}
-				
-				//TODO: make the status pretty: https://git-scm.com/docs/git-status
-				string pathBlurbRemoved = Regex.Replace(_lastStatus, "^Running: 'git status --porcelain' in '.*'", "");
-
-				if (pathBlurbRemoved.Contains("status"))
-				{
-					Debug.Log(pathBlurbRemoved);
-				}
-				
-				string[] lines = pathBlurbRemoved.Split('\n');
-				
-				int lineCount = 0;
-				string formatttedStatus = String.Empty;
-				int numMetaChanged = 0;
-				for(int i = 0; i < lines.Length; i++)
-				{
-					if (string.IsNullOrEmpty(lines[i]))
-					{
-						continue;
-					}
-
-					if (lines[i].Trim('\"',' ', '\r').EndsWith(".meta") && collapse)
-					{
-						numMetaChanged++;
-						continue;
-					}
-					
-					//Prettify common outputs
-					string line = lines[i].Trim();
-					foreach (var kvp in _statusPrettifyLookup)
-					{
-						if (line.StartsWith(kvp.Key))
-						{
-							line = $"{kvp.Value} {line.Substring(kvp.Key.Length)}";
-							break;
-						}
-					}
-					
-					formatttedStatus += $"{line.Trim()}\n";
-					lineCount++;
-				}
-
-				if (numMetaChanged > 0)
-				{
-					formatttedStatus += $"{numMetaChanged} Meta file changes detected.\n";
-					lineCount++;
-				}
-				
-				
-				HasUncommittedChanges = lineCount > 1;
-					
-				return $"{formatttedStatus.Trim('\r', '\n', ' ')}";
 			}
 		}
-
+		
 		public volatile bool HasUncommittedChanges;
 
 		//TODO: add this functionality!
@@ -153,6 +90,75 @@ namespace GitRepositoryManager
 		//TODO: add this functionality!
 		public volatile bool BehindOrigin;
 
+		private static object lockObject = new object();
+		private static object statusLockObject = new object();
+
+		private List<KeyValuePair<string, string>> _statusPrettifyLookup = new List<KeyValuePair<string, string>>()
+		{
+			new KeyValuePair<string, string>("D ", "Deleted"),
+			new KeyValuePair<string, string>("?? ", "Untracked"),
+			new KeyValuePair<string, string>("M ", "Modified")
+		};
+
+		///This is called from multiple threads! May block.
+		public string CreatePrintableStatus(bool collapse)
+		{
+			if (_lastStatus == "Repository not found.")
+			{
+				return $"{_lastStatus}";
+			}
+			
+			//TODO: make the status pretty: https://git-scm.com/docs/git-status
+			string pathBlurbRemoved = Regex.Replace(_lastStatus.Replace("\r", ""), "^Running: '(.|\n)*' in '.*'", "", RegexOptions.Multiline);
+
+			string[] lines = new string[0];
+			if (!string.IsNullOrEmpty(pathBlurbRemoved))
+			{
+				lines = pathBlurbRemoved.Split('\n');
+			}
+		
+			int lineCount = 0;
+			string formatttedStatus = String.Empty;
+			int numMetaChanged = 0;
+			for(int i = 0; i < lines.Length; i++)
+			{
+				string line = lines[i].Trim(' ', '\r', '\n', '\t');
+				if (string.IsNullOrEmpty(line))
+				{
+					continue;
+				}
+
+				if (line.Trim('\"').EndsWith(".meta") && collapse)
+				{
+					numMetaChanged++;
+					continue;
+				}
+			
+				//Prettify common outputs
+				foreach (var kvp in _statusPrettifyLookup)
+				{
+					if (line.StartsWith(kvp.Key))
+					{
+						line = $"{kvp.Value} {line.Substring(kvp.Key.Length)}";
+						break;
+					}
+				}
+			
+				formatttedStatus += $"{line.Trim()}\n";
+				lineCount++;
+			}
+
+			if (numMetaChanged > 0)
+			{
+				formatttedStatus += $"{numMetaChanged} Meta file changes detected.\n";
+				lineCount++;
+			}
+		
+		
+			HasUncommittedChanges = lineCount > 0;
+			
+			return $"{formatttedStatus.Trim('\r', '\n', ' ')}";
+		}
 		public class RepoState
 		{
 			public string Url; //Remote repo base
@@ -171,7 +177,7 @@ namespace GitRepositoryManager
 
 		//shared between thread pool process and main thread
 		private volatile bool _inProgress;
-		private volatile bool _refreshPending = false;
+		private volatile bool _refreshPending;
 
 		public string Branch => _state.Branch;
 
@@ -271,7 +277,6 @@ namespace GitRepositoryManager
 
 		public bool TryRemoveCopy()
 		{
-
 			if (!Directory.Exists(AbsolutePath))
 			{
 				return false;
@@ -328,35 +333,34 @@ namespace GitRepositoryManager
 		/// <param name="stateInfo"></param>
 		private void StatusTask(object stateInfo)
 		{
-			//Do as much as possible outside of unity so we dont get constant rebuilds. Only when everything is ready
-			RepoState state = (RepoState)stateInfo;
-
-			if(state == null)
+			lock (lockObject)
 			{
-				_progressQueue.Enqueue(new Progress(0, "Repository state info is null",true));
-				return;
-			}
+				//Do as much as possible outside of unity so we dont get constant rebuilds. Only when everything is ready
+				RepoState state = (RepoState) stateInfo;
 
-			if (GitProcessHelper.RepositoryIsValid(state.RepositoryFolder, OnProgress))
-			{
-				lock (_lastStatus)
+				if (state == null)
 				{
-					_lastStatus = GitProcessHelper.Status(state.RootFolder, state.RepositoryFolder, OnProgress);
+					_progressQueue.Enqueue(new Progress(0, "Repository state info is null", true));
+					return;
 				}
-			}
-			else
-			{
-				lock (_lastStatus)
-				{
-					_lastStatus = "Repository not found.";
-				}
-			}
-			
-			_lastPrintableStatus = CreatePrintableStatus(true);
 
-			void OnProgress(bool success, string message)
-			{
-				_progressQueue.Enqueue(new Progress(0, message, !success));
+				lock (statusLockObject)
+				{
+					if (GitProcessHelper.RepositoryIsValid(state.RepositoryFolder, OnProgress))
+					{
+						_lastStatus = GitProcessHelper.Status(state.RootFolder, state.RepositoryFolder, OnProgress);
+					}
+					else
+					{
+						_lastStatus = "Repository not found.";
+					}
+					_lastPrintableStatus = CreatePrintableStatus(true);
+				}
+				
+				void OnProgress(bool success, string message)
+				{
+					_progressQueue.Enqueue(new Progress(0, message, !success));
+				}
 			}
 		}
 		
@@ -366,95 +370,10 @@ namespace GitRepositoryManager
 		/// <param name="stateInfo"></param>
 		private void ClearLocalChangesTask(object stateInfo)
 		{
-			//Do as much as possible outside of unity so we dont get constant rebuilds. Only when everything is ready
-			RepoState state = (RepoState)stateInfo;
-
-			if(state == null)
-			{
-				_progressQueue.Enqueue(new Progress(0, "Repository state info is null",true));
-				return;
-			}
-
-			if (GitProcessHelper.RepositoryIsValid(state.RepositoryFolder, OnProgress))
-			{
-				GitProcessHelper.ClearLocalChanges(state.RootFolder,state.RepositoryFolder, 
-					state.DirectoryInRepository, state.Url, state.Branch, OnProgress);
-				_refreshPending = true;
-			}
-			else
-			{
-				lock (_lastStatus)
-				{
-					_progressQueue.Enqueue(new Progress(0, "Repository not found",true));
-				}
-			}
-			
-			_lastPrintableStatus = CreatePrintableStatus(true);
-
-			void OnProgress(bool success, string message)
-			{
-				_progressQueue.Enqueue(new Progress(0, message, !success));
-			}
-		}
-
-		/// <summary>
-		/// Runs in a thread pool. Should clone then checkout the appropriate branch/commit. copy subdirectory into specified repo.
-		/// </summary>
-		/// <param name="stateInfo"></param>
-		private void UpdateTask(object stateInfo)
-		{
-			try
+			lock (lockObject)
 			{
 				//Do as much as possible outside of unity so we dont get constant rebuilds. Only when everything is ready
-				RepoState state = (RepoState)stateInfo;
-
-				if(state == null)
-				{
-					_progressQueue.Enqueue(new Progress(0, "Repository state info is null",true));
-					return;
-				}
-
-				if (GitProcessHelper.RepositoryIsValid(state.RepositoryFolder, OnProgress))
-				{
-					GitProcessHelper.UpdateRepository(state.RootFolder,state.RepositoryFolder, state.DirectoryInRepository, state.Url, state.Branch, OnProgress);
-				}
-				else
-				{
-					GitProcessHelper.AddRepository(state.RootFolder, state.RepositoryFolder, state.DirectoryInRepository, state.Url, state.Branch, OnProgress);
-				}
-
-				//Once completed
-				if (_progressQueue.Count > 0)
-				{
-					//Get the latest progress
-					if (!_progressQueue.ToArray()[_progressQueue.Count-1].Error)
-					{
-						_refreshPending = true;
-					}
-				}
-
-				_inProgress = false;
-
-				void OnProgress(bool success, string message)
-				{
-					_progressQueue.Enqueue(new Progress(0, message, !success));
-				}
-			}
-			catch (Exception e)
-			{
-				Debug.LogError($"[Repository.UpdateTask] {e.Message}");
-			}
-		}
-
-		/// <summary>
-		/// Runs in a thread pool. Should push based on settings of push window. copy subdirectory into specified repo.
-		/// </summary>
-		/// <param name="stateInfo"></param>
-		private void PushTask(object stateInfo)
-		{
-			try
-			{
-				PushState state = (PushState) stateInfo;
+				RepoState state = (RepoState) stateInfo;
 
 				if (state == null)
 				{
@@ -464,41 +383,140 @@ namespace GitRepositoryManager
 
 				if (GitProcessHelper.RepositoryIsValid(state.RepositoryFolder, OnProgress))
 				{
-					GitProcessHelper.CheckoutBranch(state.RootFolder, state.RepositoryFolder, state.DirectoryInRepository,
-						state.Url, state.Branch, OnProgress);
-					GitProcessHelper.PullMerge(state.RootFolder, state.RepositoryFolder, state.DirectoryInRepository,
-						state.Url, state.Branch, OnProgress);
-					GitProcessHelper.Commit(state.RootFolder, state.RepositoryFolder, state.DirectoryInRepository,
-						state.Url, state.Message, OnProgress);
-					GitProcessHelper.PushRepository(state.RootFolder, state.RepositoryFolder,
+					GitProcessHelper.ClearLocalChanges(state.RootFolder, state.RepositoryFolder,
 						state.DirectoryInRepository, state.Url, state.Branch, OnProgress);
+					_refreshPending = true;
 				}
-
-				//Once completed
-				if (_progressQueue.Count > 0)
+				else
 				{
-					//Get the latest progress
-					if (!_progressQueue.ToArray()[_progressQueue.Count - 1].Error)
-					{
-						_refreshPending = true;
-					}
+					_progressQueue.Enqueue(new Progress(0, "Repository not found", true));
 				}
 
-				_inProgress = false;
-
+				lock (statusLockObject)
+				{
+					_lastPrintableStatus = CreatePrintableStatus(true);
+				}
+				
 				void OnProgress(bool success, string message)
 				{
 					_progressQueue.Enqueue(new Progress(0, message, !success));
 				}
 			}
-			catch (Exception e)
+		}
+
+		/// <summary>
+		/// Runs in a thread pool. Should clone then checkout the appropriate branch/commit. copy subdirectory into specified repo.
+		/// </summary>
+		/// <param name="stateInfo"></param>
+		private void UpdateTask(object stateInfo)
+		{
+			lock (lockObject)
 			{
-				Debug.LogError($"[Repository.PushTask] {e.Message}");
+				try
+				{
+					//Do as much as possible outside of unity so we dont get constant rebuilds. Only when everything is ready
+					RepoState state = (RepoState) stateInfo;
+
+					if (state == null)
+					{
+						_progressQueue.Enqueue(new Progress(0, "Repository state info is null", true));
+						return;
+					}
+
+					if (GitProcessHelper.RepositoryIsValid(state.RepositoryFolder, OnProgress))
+					{
+						GitProcessHelper.UpdateRepository(state.RootFolder, state.RepositoryFolder,
+							state.DirectoryInRepository, state.Url, state.Branch, OnProgress);
+					}
+					else
+					{
+						GitProcessHelper.AddRepository(state.RootFolder, state.RepositoryFolder,
+							state.DirectoryInRepository, state.Url, state.Branch, OnProgress);
+					}
+
+					//Once completed
+					if (_progressQueue.Count > 0)
+					{
+						//Get the latest progress
+						if (!_progressQueue.ToArray()[_progressQueue.Count - 1].Error)
+						{
+							_refreshPending = true;
+						}
+					}
+
+					_inProgress = false;
+
+					void OnProgress(bool success, string message)
+					{
+						_progressQueue.Enqueue(new Progress(0, message, !success));
+					}
+				}
+				catch (Exception e)
+				{
+					Debug.LogError($"[Repository.UpdateTask] {e.Message}");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Runs in a thread pool. Should push based on settings of push window. copy subdirectory into specified repo.
+		/// </summary>
+		/// <param name="stateInfo"></param>
+		private void PushTask(object stateInfo)
+		{
+			lock (lockObject)
+			{
+				try
+				{
+					PushState state = (PushState) stateInfo;
+
+					if (state == null)
+					{
+						_progressQueue.Enqueue(new Progress(0, "Repository state info is null", true));
+						return;
+					}
+
+					if (GitProcessHelper.RepositoryIsValid(state.RepositoryFolder, OnProgress))
+					{
+						GitProcessHelper.CheckoutBranch(state.RootFolder, state.RepositoryFolder,
+							state.DirectoryInRepository,
+							state.Url, state.Branch, OnProgress);
+						GitProcessHelper.PullMerge(state.RootFolder, state.RepositoryFolder,
+							state.DirectoryInRepository,
+							state.Url, state.Branch, OnProgress);
+						GitProcessHelper.Commit(state.RootFolder, state.RepositoryFolder, state.DirectoryInRepository,
+							state.Url, state.Message, OnProgress);
+						GitProcessHelper.PushRepository(state.RootFolder, state.RepositoryFolder,
+							state.DirectoryInRepository, state.Url, state.Branch, OnProgress);
+					}
+
+					//Once completed
+					if (_progressQueue.Count > 0)
+					{
+						//Get the latest progress
+						if (!_progressQueue.ToArray()[_progressQueue.Count - 1].Error)
+						{
+							_refreshPending = true;
+						}
+					}
+
+					_inProgress = false;
+
+					void OnProgress(bool success, string message)
+					{
+						_progressQueue.Enqueue(new Progress(0, message, !success));
+					}
+				}
+				catch (Exception e)
+				{
+					Debug.LogError($"[Repository.PushTask] {e.Message}");
+				}
 			}
 		}
 
 
 		//TODO: redo for whole folder
+		//TODO: unused. Remove?
 		private void SetIgnore(string parentRoot, string relativeFolderToIgnore, bool ignore)
 		{
 			//This is a pretty ok attempt at automating adding to gitignore. If people go in and add to the ignore manually this may not pick that up but that's fine.
